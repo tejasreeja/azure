07feba62ae80b96520a082dcd13686b282b24509
Venkat Ambati <tejasreeja@yahoo.com>
2022-09-11 03:53:31 +0000

    Pizza

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {}
+
+config.endpoint = 'https://va-cosmosdb.documents.azure.com:443/'
+config.key = 'lJiVgFw7BNvOD67bpTuJ88tJuENJHvmoMcTr5ECWWMWbS3hIdX9iEbMgWxpzggGMVMDhLW5ab0IBeIHb05FsEQ=='
+
+config.database = { id: 'pizzadb' }
+config.container = { id: 'pizzas' }
+
+module.exports = config;
